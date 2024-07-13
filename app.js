cd9a55037def27fd76b20fd6ada7a4d09b1e3847
subhajit roy <rsubhajit024@gmail.com>
2024-07-13 21:38:27 +0530

    ignite our app

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
 const heading1 = React.createElement("h1",{
     id:"title",
 },
@@ -10,10 +14,13 @@ const heading2 = React.createElement("h2",{
 
 const container = React.createElement("div",{
     id:"container"
-},[heading1,heading2])
+},[heading1,heading2]);
  
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //passing a react element inside the root
 root.render(container);
+
+
+
